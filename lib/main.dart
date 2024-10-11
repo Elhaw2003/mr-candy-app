b0a59448fff017b8e65d8c17ca86e1a5fc09efa9
Elhaw2003 <ebrahimelhaw0@gmail.com>
2024-10-12 02:24:15 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MrCandyApp());
+}
+class MrCandyApp extends StatelessWidget {
+  const MrCandyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
